37c64a8122c79ca559e084e55af4ba118d81483d
Steven++ <steven_plus_plus@outlook.com>
2018-07-08 09:46:18 +0800

    add hotkeys

--- query.js
+++ query.js
@@ -1,6 +1,12 @@
 function get(id) {
     return document.getElementById(id);
 }
+document.onkeydown = function (ev) {
+    ev = ev || window.event;
+    if (ev.keyCode == 13) {
+        query();
+    }
+}
 var key = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJiMjIzMWRiMC02M2VjLTAxMzYtMDc0My0wYTU4NjQ2MGI5MjkiLCJpc3MiOiJnYW1lbG9ja2VyIiwiaWF0IjoxNTMwOTUxNjk5LCJwdWIiOiJzZW1jIiwidGl0bGUiOiJ2YWluZ2xvcnkiLCJhcHAiOiJ2Z3F1ZXJ5Iiwic2NvcGUiOiJjb21tdW5pdHkiLCJsaW1pdCI6MTB9.I2y8hWOA_IKT8VGCeN2qEoJYT8wuSTDzDy1esjIBuBQ';
 function request(url, fn) {
     var req = new XMLHttpRequest();
